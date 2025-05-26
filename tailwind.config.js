76917b8184f9e82561f4940d6cf0103e83937769
Francisco Ortega <jf.ortega496@gmail.com>
2025-05-26 17:34:57 -0600

    UI: Implementación de tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html",
+    "./app_artesanos/templates/**/*.html",
+    "./static/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} 
